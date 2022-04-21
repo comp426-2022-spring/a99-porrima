56cc2f214e5264c6f3a7731c5712ac890f2eeb9a
Nifu Dan <dannifu010706@outlook.com>
2022-04-21 10:31:54 -0400

    bmi calculator

---
+++ bmi-calculator/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import BMICal from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BMICal />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+

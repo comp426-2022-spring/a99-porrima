5cd17cf1dc27eda39fa9567ab918344fc9015e40
Lama Abed <lamaab@email.unc.edu>
2022-04-25 04:25:56 -0400

    done with home page

--- porrima-app/src/components/Home/home.jsx
+++ porrima-app/src/components/Home/home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import  "./home.css";
+import vectors from "../../assets/funcolors.png"
 
 function home() {
   return (
-    <div>home</div>
+    <div className="header_padding">
+      <div className="header_content">
+         <h1 className='gradient-text'>Welcome To Your Health Journal Website!</h1>
+         <p>Start with loging in your health entery for today!</p>
+      </div>
+      <div className="header_img">
+        <img src={vectors} alt="shapes"/>
+      </div>
+    </div>
   )
 }
 

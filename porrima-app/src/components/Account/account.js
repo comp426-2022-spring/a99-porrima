96d452ac5c9619f5a8a67f3e79bc5ec7f1ca9d13
Matthew Gilmore <mattsg@email.unc.edu>
2022-04-29 20:42:00 -0400

    account page

--- porrima-app/src/components/Account/account.js
+++ porrima-app/src/components/Account/account.js
@@ -60,7 +60,7 @@ function Account() {
     const em = data.email;
 
     const username = document.getElementById("username");
-    username.classList.add("account_info");
+    username.classList.add("info");
     username.innerHTML = user;
     const email = document.getElementById("email");
     email.innerHTML = em;
@@ -84,12 +84,11 @@ function Account() {
         <h2>Update Account Info</h2>
         <form onSubmit={handleSubmit}>
           <label>
-            <p>Username</p>
-            <input type="text" onChange={(e) => setUsername(e.target.value)} />
+            <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
           </label>
+          <br></br>
           <label>
-            <p>Email</p>
-            <input type="text" onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
           </label>
           <div className="update">
             <button className="update-button" type="submit">
